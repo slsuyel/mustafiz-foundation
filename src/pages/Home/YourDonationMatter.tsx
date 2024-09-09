fd94468c4b86290ee17fb9e4d02503f2c8609611
slsuyel <slsuyel@gmail.com>
2024-09-09 16:28:16 +0600

    fix: bugs

--- src/pages/Home/YourDonationMatter.tsx
+++ src/pages/Home/YourDonationMatter.tsx
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom';
 
 const YourDonationMatter = () => {
   return (
-    <div className=" container mx-auto my-32">
-      <div className="flex items-center">
+    <div className="container mx-auto my-32">
+      <div className="flex items-center flex-wrap">
         <div>
           <img
+            className="w-full"
             src={
               'https://images.unsplash.com/photo-1593113598332-cd288d649433?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
             }
